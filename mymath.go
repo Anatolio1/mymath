2fc88280c9cca9ac2dfeeda71a00a98e5e78ced4
Anatoly Smolsky <fygas333@gmail.com>
2024-07-12 16:00:22 +0300

    mymath v1.0.0

---
+++ mymath.go
@@ -0,0 +1,34 @@
+package mymath
+
+import "math"
+
+func Sqrt(num float64) float64 {
+	return math.Sqrt(num)
+}
+func Ceil(num float64) float64 {
+	return math.Ceil(num)
+}
+func Floor(num float64) float64 {
+	return math.Floor(num)
+}
+func Pow(num float64, power float64) float64 {
+	return math.Pow(num, power)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
